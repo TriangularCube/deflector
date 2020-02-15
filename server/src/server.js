5228a11ab603fe5d9ff6992a61670f9316395053
Michael Liu <michael.liu0@gmail.com>
2020-02-14 17:09:49 -0800

    Finishing up configuring directory

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,19 @@
 const http = require('http')
 
+// const schedule = require('node-schedule')
+
+// let j = schedule.scheduleJob( '*/1 * * * * *', () => {
+//     console.log('tick')
+// })
+
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain')
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1234')
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1234 https://www.deflector.io')
     res.end('This is it!')
 })
 
 server.listen( 3000, () => {
     console.log('Server is running')
+    console.log(process.env.NODE_ENV)
 })
