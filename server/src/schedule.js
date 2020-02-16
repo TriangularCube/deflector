e700d2e9f60fdc0899ca25b2cfb9bc0cbb076c62
Michael Liu <michael.liu0@gmail.com>
2020-02-16 00:10:25 -0800

    server: reset schedule timer

--- server/src/schedule.js
+++ server/src/schedule.js
@@ -3,7 +3,7 @@ const schedule = require('node-schedule')
 let rule = new schedule.RecurrenceRule()
 
 rule.hour = new schedule.Range(0, 23, 2)
-rule.minute = 5
+rule.minute = 0
 
 let job = schedule.scheduleJob( rule, () => {
     console.log('tick')
