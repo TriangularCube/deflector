80bb1da18b975db1fcd56f15c61517113b005c76
Michael Liu <mikeliu@evidentpoint.com>
2021-02-08 19:58:47 -0800

    Preliminary scheduled event test

--- services/api/test/test.js
+++ services/api/test/test.js
@@ -1,11 +1,11 @@
 module.exports.main = async (event) => {
-    return {
-        statusCode: 200,
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify({
-            success: true
-        }, null, 2),
-    }
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      success: true
+    }, null, 2),
+  }
 }
---
+++ services/creator/classic/creator.js
@@ -0,0 +1,4 @@
+module.exports.handler = (event) => {
+  console.log('Creator!!')
+  console.log(event)
+}
